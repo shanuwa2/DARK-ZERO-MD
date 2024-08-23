d82f16b9008046840959be7fe1c6b0832d25a3db
Navinofc44 <Pahasarasanujja@gmail.com>
2024-08-23 05:58:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || "",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/7e1f9a45d326abc34d161.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "*_Hello there 👋_**_🙌 I am SADARU-MD WhatsApp User Bot_*",
 };
