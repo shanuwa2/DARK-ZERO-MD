56547dc81a12642a96cf866f54608840b1954be6
Navinofc44 <Pahasarasanujja@gmail.com>
2024-08-31 20:14:51 +0530

    Update autosticker.js

--- my_data/autosticker.js
+++ my_data/autosticker.js
@@ -1,7 +1,7 @@
 {
-"hi":"example hi url",
-"mk":"example mk url",
-"gm":"example gm url",
-"gn":"example gn url",
-"sad":"example sad url"
+"hi":"https://github.com/Navinofc44/EXAMPLE_DATABASE/blob/main/autosticker/bd5914df69a865b3f3169a36c8507005.jpg",
+"mk":"https://github.com/Navinofc44/EXAMPLE_DATABASE/blob/main/autosticker/200w.gif",
+"gm":"https://github.com/Navinofc44/EXAMPLE_DATABASE/blob/main/autosticker/VID-20240831-WA0031.mp4",
+"gn":"https://github.com/Navinofc44/EXAMPLE_DATABASE/blob/main/autosticker/goodnight-kitty.gif",
+"sad":"https://github.com/Navinofc44/EXAMPLE_DATABASE/blob/main/autosticker/IMG-20240730-WA0002.jpg"
 }
