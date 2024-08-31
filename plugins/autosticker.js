9d833e29bbf263a1991d1f1270ec1816de393647
Navinofc44 <Pahasarasanujja@gmail.com>
2024-08-31 18:28:38 +0530

    Create autosticker.js

---
+++ plugins/autosticker.js
@@ -0,0 +1,18 @@
+//auto sticker 
+cmd({
+  on: "body"
+},    
+async (conn, mek, m, { from, body, isOwner }) => {
+    const filePath = path.join(__dirname, '../my_data/autosticker.json');
+    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    for (const text in data) {
+        if (body.toLowerCase() === text.toLowerCase()) {
+            const config = await readEnv();
+            if (config.AUTO_STICKER === 'true') {
+                //if (isOwner) return;        
+                await conn.sendMessage(from,{sticker: { url : data[text]},package: 'DARK ZERO MD'},{ quoted: mek })   
+            
+            }
+        }
+    }                
+});
