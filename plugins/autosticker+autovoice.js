dffec44f209da8f20e3a74de9bdfa793dab41df3
Navinofc44 <Pahasarasanujja@gmail.com>
2024-09-01 08:36:38 +0530

    Update and rename autosticker+autovoice+autoreply.js to autosticker+autovoice.js

--- plugins/autosticker+autovoice.js
+++ plugins/autosticker+autovoice.js
@@ -35,22 +35,3 @@ async (conn, mek, m, { from, body, isOwner }) => {
         }
     }                
 });
-
-//auto reply 
-cmd({
-  on: "body"
-},    
-async (conn, mek, m, { from, body, isOwner }) => {
-    const filePath = path.join(__dirname, '../my_data/autoreply.json');
-    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-    for (const text in data) {
-        if (body.toLowerCase() === text.toLowerCase()) {
-            const config = await readEnv();
-            if (config.AUTO_REPLY === 'true') {
-                //if (isOwner) return;        
-                await m.reply(data[text])
-            
-            }
-        }
-    }                
-});
