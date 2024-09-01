ed1d3490d6937953e96cd4bb7be6ff754d3ba71c
Navinofc44 <Pahasarasanujja@gmail.com>
2024-09-01 15:16:16 +0530

    Update autovoice.js

--- my_data/autovoice.js
+++ my_data/autovoice.js
@@ -1,4 +1,4 @@
 {
-"alive":""
-"menu":""
+"alive":"https://github.com/Navinofc44/EXAMPLE_DATABASE/raw/main/autovoice/AUD-20240901-WA0160.mp3"
+"menu":"https://github.com/Navinofc44/EXAMPLE_DATABASE/raw/main/autovoice/AUD-20240901-WA0161.mp3"
 }
