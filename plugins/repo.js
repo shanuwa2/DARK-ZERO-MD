23094d43e501f0e128b6b81a302626f0dab33ca0
Navinofc44 <Pahasarasanujja@gmail.com>
2024-09-04 14:35:59 +0530

    Update repo.js

--- plugins/repo.js
+++ plugins/repo.js
@@ -11,15 +11,15 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-let dec = `*💫ℝ𝔼ℙ𝕆-𝕃𝕀ℕ𝕂 💗👇*
+let dec = `*🧸ℝ𝔼ℙ𝕆-𝕃𝕀ℕ𝕂 ⭐🔮*
 
-🧚‍♀️◦https://github.com/Navinofc44/DARK-ZERO-MD
+💕◦https://github.com/Navinofc44/DARK-ZERO-MD
 
-* *💫ℙ𝕃𝔼𝔸𝕊𝔼 𝐉𝐎𝐈𝐍 𝕄𝕐 𝕎ℍ𝔸𝕋𝕊𝔸ℙℙ 𝐆𝐑𝐎𝐔𝐏 💗👇*
+* *💫ℙ𝕃𝔼𝔸𝕊𝔼 𝐉𝐎𝐈𝐍 𝕄𝕐 𝕎ℍ𝔸𝕋𝕊𝔸ℙℙ 𝐆𝐑𝐎𝐔𝐏 💛🥺*
 
-🧚‍♀️◦ https://chat.whatsapp.com/B99RDjud9r98m7AqiUUjR7
+💕◦ https://chat.whatsapp.com/B99RDjud9r98m7AqiUUjR7
 
-*©𝐷𝐴𝑅𝐾 𝑍𝐸𝑅𝑂-ᴍᴅ ʙʏ ᗪᗩᖇK ᕼᗩᑕKEᖇ Tᗰッ*
+*©𝐷𝐴𝑅𝐾 𝑍𝐸𝑅𝑂-ᴍᴅ ʙʏ ᗪᗩᖇK ᕼᗩᑕKEᖇ Tᗰッ💚🙃*
 `
 await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/7e1f9a45d326abc34d161.jpg`},caption:dec},{quoted:mek});
 
