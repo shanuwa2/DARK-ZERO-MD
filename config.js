7929a21253eeb4ebc716c09a5c0e59cf861d7fbe
Cyber570 <150837648+Cyber570@users.noreply.github.com>
2024-09-06 06:16:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "1iExgAJD#eXwpGs8JORTX8x7bOEGeBxwkmFbyqKqvVo6nmEGFIhA",
+SESSION_ID: process.env.SESSION_ID || "wq0mHRLD#_9GGlHH7YGQ4hheNwyxcmuxHQjsksM0Ga1hBOBAqW8M",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/7e1f9a45d326abc34d161.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "*👋 Hello ${pushname}* ✫💫 𝐃𝐀𝐑𝐊 𝐙𝐄𝐑𝐎 𝐌𝐃 💫✫\n\n*Hey there!*\n\n 💫 *∂αяκ zєяο  ωнαтѕαρρ вσт ιѕ υρ αη∂ яυηηιηg!*\n🖤 *яυηтιмє : 1 hour, 5 minutes, 47 seconds\n🤍 *¢яєαтє∂ ву:* DARK ZERO HACKER TM\n\n*𝐻𝑒𝓇𝑒'𝓈 𝓌𝒽𝒶𝓉 𝐼 𝒸𝒶𝓃 𝒹𝑜*\n💿 *Download Songs & Videos*\n📰 *Fetch Latest News*\n🎭 *Entertain with Fun Commands*\n🔧 *Manage Groups*\n\n*𝒮𝓉𝒶𝓎 𝒸𝑜𝓃𝓃𝑒𝒸𝓉𝑒𝒹 𝒶𝓃𝒹 𝑒𝓃𝒿𝑜𝓎 𝓉𝒽𝑒 𝓈𝑒𝓇𝓋𝒾𝒸𝑒𝓈!*\n\n💕𝙃𝙚𝙡𝙥𝙚𝙧𝙨💕 𝗠𝗔𝗡𝗨𝗟 𝗢𝗙𝗙𝗜𝗖𝗜𝗔𝗟\n𝗛𝗔𝗡𝗦𝗔 𝗗𝗘𝗪𝗠𝗜𝗡𝗔\n𝗡𝗜𝗥𝗢\n\n_*© 𝗗𝗔𝗥𝗞 𝗭𝗘𝗥𝗢 𝘽𝙊𝙏 - MD*_\n\n💻 *GitHub:* https://github.com/Navinofc44/DARK-ZERO-MD",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
